587ee025fb0a2fd874f7148088c51c8531a5845f
jangjiheee <jihee.jang210@gmail.com>
2022-06-10 17:43:28 +0900

    6월 10일 햄버거버튼

---
+++ js/hamburger.js
@@ -0,0 +1,26 @@
+// 햄버거 
+/*window.onload = function() {
+    const hamburger = document.getElementById("menu");
+    const menuBar = document.querySelector("mobile-menu");
+    
+    $(hamburger).on("click", function() {
+        $(menuBar).slideDown(500);
+    });
+}*/
+
+// 모바일 햄버거 버튼
+const hamburger = document.getElementById("menu");
+const menuBar = document.querySelector(".mobile-gnb");
+
+let subToggle = true;
+function showMenu(){
+    if(subToggle) {
+        menuBar.style.setProperty('height', 'calc(100vh - 80px)');
+        subToggle =! subToggle;
+      } else {
+        menuBar.style.height = "0px";
+        subToggle =! subToggle;
+    }
+}
+
+hamburger.addEventListener("click", showMenu);
