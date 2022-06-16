7bb05f933ee8c3f243d1e1f180babc771c41fb9b
jangjiheee <jihee.jang210@gmail.com>
2022-06-16 17:44:00 +0900

    6월 16일 수정

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
 
+    
     $(window).scroll( function(){
 
-        $('#main-nav').each( function(i){
+        $('#main-nav').each( function(){
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_object ){
                 $(this).animate({'opacity':'1'},500);
+            } else {
+                $(this).animate({'opacity':'0'},500)
             }
         });
     });
+
 });
+
+    // $(window).on("scroll", function() {
+    //     if($(this).scrollTop() > 100) {
+    //         $("#mainNav").fadeIn();
+    //     } else {
+    //         $("#mainNav").fadeOut();
+    //     }
+    // })
+
+
+/*$(document).ready(function() {
+    $("#main-nav").hide()
+    $(function() {
+        $(window).scroll(function() {
+            if($(this).scrollTop()>1000) {
+                $("#main-nav").fadeIn()
+            }
+        })
+    })
+})*/
---
+++ js/scrollAction.js
@@ -0,0 +1,28 @@
+/*AOS.init({
+    easing: 'ease-in-out-sine'
+});
+
+setInterval(addIetm,300);*/
+document.getElementById('photoImg')
+.setAttribute('data-aos-duration', '2000');
+
+
+document.querySelector('.explain')
+.setAttribute('data-aos-duration', '1500');
+
+window.onload = function() {
+    var skill = document.getElementById("skillImg");
+    var skillChildren = skill.children;
+
+    // skillChildren[0].setAttribute('data-aos-duration', '2500');
+    // skillChildren[1].setAttribute('data-aos-duration', '2500');
+    // skillChildren[2].setAttribute('data-aos-duration', '2500');
+    // skillChildren[3].setAttribute('data-aos-duration', '2500');
+    // skillChildren[4].setAttribute('data-aos-duration', '2500');
+    // skillChildren[5].setAttribute('data-aos-duration', '2500');
+    // skillChildren[6].setAttribute('data-aos-duration', '2500');
+    // skillChildren[7].setAttribute('data-aos-duration', '2500');
+    for(let i=0; i<skillChildren.length; i++) {
+        skillChildren[i].setAttribute('data-aos-duration', '2500')
+    }
+}
