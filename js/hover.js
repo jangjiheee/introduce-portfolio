015a0cd776098b802e2ccca0a7a9ea8067f1908e
jangjiheee <jihee.jang210@gmail.com>
2022-06-26 11:38:15 +0900

    hover효과 만들기

---
+++ js/hover.js
@@ -0,0 +1,9 @@
+$(function() {
+    var rect = document.getElementsByClassName('rectangle');
+    var hoverT = document.getElementsByClassName('skill-hover');
+
+    console.log(rect);
+    console.log(hoverT);
+
+    hoverT.hide();
+})
